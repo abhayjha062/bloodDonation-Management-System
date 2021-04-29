19221c5eeaebfcfc0854742f3feecf8f26d04ede
Abhay Kumar Jha <abhayjha062@gmail.com>
2021-04-29 17:22:13 +0530

    About page

--- about.php
+++ about.php
@@ -22,7 +22,7 @@
     <div class="container mt-3">
       <div class="jumbotron">
        <h1>About Us</h1>      
-       <p>We are group of individuals with a small idea of helping needy people who is in the need of blood We are providing a platform where a donor can come and register himself with thier info and help the scociety in donating blood in thier area and the needy people will search for thier respective blood group on our platform and will contact the person matching the blood group for same In this way we can save the live of thousands of people who is in the need of donor and couldn't find the same in mean time Any people in any location can get benifitted by this So guys come forward and register here and make a effort to help the scociety and people who is need for blood</p>
+       <p>We are group of individuals with a small idea of helping needy people who is in the need of blood We are providing a platform where a donor can come and register himself with thier info and help the scociety in donating blood in thier area and the needy people will search for thier respective blood group on our platform and will contact the person matching the blood group for same In this way we can save the live of thousands of people who is in the need of donor and couldnt find the same in mean time Any people in any location can get benifitted by this So guys come forward and register here and make a effort to help the scociety and people who is need for blood</p>
       </div>   
     </div>
 
